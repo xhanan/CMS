db167af4c4e310ffca1a34d53b3fb14951da468d
ylber-gashi <ylber.gashi@studentet.uni-pr.edu>
2020-04-19 16:31:54 +0200

    Localhost .1

--- dbConnection.php
+++ dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-    define("DB_HOST","127.0.0.4:3307");
+    define("DB_HOST","127.0.0.1:3307");
     define("DB_USER","root");
     define("DB_PASS","");
     define("DB_NAME","brainbyte");

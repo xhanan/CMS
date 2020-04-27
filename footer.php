45b0c9db5a2caebe58c0402cf2c9302bed05afe3
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-04-27 16:32:33 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -95,12 +95,12 @@
 <div class="container-fluid fh5co_footer_right_reserved">
     <div class="container">
         <div class="row  ">
-            <div class="col-12 col-md-6 py-4 Reserved"> © Copyright 2018, All rights reserved. Design by <a href="https://freehtml5.co" title="Free HTML5 Bootstrap templates">FreeHTML5.co</a>. </div>
+            <div class="col-12 col-md-6 py-4 Reserved"> © Copyright 2020, All rights reserved. Design by XYZ. </div>
             <div class="col-12 col-md-6 spdp_right py-4">
-                <a href="index.html" class="footer_last_part_menu">Home</a>
-                <a href="Contact_us.html" class="footer_last_part_menu">About</a>
-                <a href="Contact_us.html" class="footer_last_part_menu">Contact</a>
-                <a href="blog.html" class="footer_last_part_menu">Latest News</a></div>
+                <a href="index.php" class="footer_last_part_menu">Home</a>
+                <a href="Contact_us.php" class="footer_last_part_menu">About</a>
+                <a href="Contact_us.php" class="footer_last_part_menu">Contact</a>
+                <a href="blog.php" class="footer_last_part_menu">Latest News</a></div>
         </div>
     </div>
 </div>

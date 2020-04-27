f9c6f51b8813c3e8e163c0f14ca1616a9cdd66f7
ylber-gashi <ylber.gashi@studentet.uni-pr.edu>
2020-04-27 14:04:26 +0200

    Ruajtja e permbajtjes se postimit ne databaze

--- post.php
+++ post.php
@@ -1,26 +1,52 @@
+
 <!DOCTYPE php>
 <php lang="en" class="no-js">
+<?php include "dbConnection.php";?>
+<?php
+
+
+if(isset($_POST['create_post'])) {
+    $title = mysqli_real_escape_string($connection, $_POST['title']);
+    $content =  mysqli_real_escape_string($connection,$_POST['content']);
+    $category = mysqli_real_escape_string($connection,$_POST['category_id']);
+    $user_id = 1;    # user id duhet te merret nga useri qe eshte i kycur (nese eshte admin)
+    $date = date('d-m-y');
+    $tags = "test";
+    
+$query = "INSERT INTO articles(title, content, category_id ,user_id, published_date, tags) ";
+
+$query .= "VALUES('{$title}','{$content}',{$category},{$user_id}, now(),'{$tags}')";
+
+$create_post_query = mysqli_query($connection, $query); 
+if(!$create_post_query){
+    die("Gabim: " . mysqli_error($connection));
+}
+else{
+    echo "postimi u ruajt";
+}
+}
+?>
 <body class="single">
 <?php include "header.php"?>
 <div id="fh5co-single-content" class="container-fluid pb-4 pt-4 paddding">
     <div class="container paddding">
         <div class="row mx-0">
             <div class="col-md-8 offset-md-2 col-12 animate-box" data-animate-effect="fadeInLeft">
-            <form>
+            <form action="post.php" method="post">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Titulli</label>
+                    <label for="title">Titulli</label>
                     <input type="text" class="form-control" name="title" placeholder="Shkruaj titullin">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Kategoria</label>
-                    <input type="password" class="form-control" name="categoryId" placeholder="Password">
+                    <label for="category_id">Kategoria</label>
+                    <input type="text" class="form-control" name="category_id" placeholder="Kategoria">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Përmbajtja</label>
-                    <textarea id="content" class="form-control" name="content" placeholder="Password">
+                    <label for="content">Përmbajtja</label>
+                    <textarea id="content" class="form-control" name="content" placeholder="Permbajtja">
                     </textarea>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" name="create_post">Submit</button>
             </form>
             </div>
         </div>

2ced26b7d7016893ef0524ec1b3d82b0040627e2
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-04-27 17:38:04 +0200

    Update blog.php

--- blog.php
+++ blog.php
@@ -41,32 +41,6 @@
                         <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
                     </div>
                 </div>
-                <!-- <div class="row pb-3">
-                    <div class="col-5 align-self-center">
-                        <img src="images/allef-vinicius-108153.jpg" alt="img" class="fh5co_most_trading"/>
-                    </div>
-                    <div class="col-7 paddding">
-                        <div class="most_fh5co_treding_font"> Enim ad minim veniam nostrud xercitation ullamco.</div>
-                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                    </div>
-                </div> -->
-                <!-- <div class="row pb-3">
-                    <div class="col-5 align-self-center">
-                        <img src="images/download (2).jpg" alt="img" class="fh5co_most_trading"/>
-                    </div>
-                    <div class="col-7 paddding">
-                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                    </div>
-                </div> -->
-                <!-- <div class="row pb-3">
-                    <div class="col-5 align-self-center"><img src="images/seth-doyle-133175.jpg" alt="img"
-                                                              class="fh5co_most_trading"/></div>
-                    <div class="col-7 paddding">
-                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                    </div>
-                </div> -->
             </div>
         </div>
         <div class="row mx-0">

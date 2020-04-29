699fbecf8416cf1981daa49c1a4219e9b1ce140e
ylber-gashi <ylber.gashi@studentet.uni-pr.edu>
2020-04-29 15:36:33 +0200

    Permiresim i gabimeve

--- header.php
+++ header.php
@@ -60,13 +60,15 @@
                                 ' . $_SESSION["user"] . '  
                                 </button>
                                 <div class="dropdown-menu">
-                                <a class="dropdown-item" href="profile.php">Profile</a>'?>
+                                <a class="dropdown-item" href="profile.php">Profile</a>'}?>
                                 <?php
                                 if(isset($_SESSION['isadmin']))
                                 '<a class="dropdown-item" href="post.php">Add Post</a>
                                 <a class="dropdown-item" href="post.php">Edit Post</a>'
                                 ?>
-                                <div class="dropdown-divider"></div>
+                                <?php if (isset($_SESSION['user'])){
+                            echo
+                                '<div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="logout.php">Log Out</a>
                                 </div>
                             </div>';

9d3548cc824385c9d1b08ffe56ad94d82e090ae0
ylber-gashi <ylber.gashi@studentet.uni-pr.edu>
2020-04-30 20:25:02 +0200

    Comment read

--- postFunctions.php
+++ postFunctions.php
@@ -17,6 +17,33 @@ function getPost($id){
 	
 	return $post;
 }
+function getComment($id){
+	global $connection;
+        $query1 = "SELECT `comments`.`descriptions`, `users`.`first_name`, `users`.`last_name`, `comments`.`datetimee`  FROM `comments` 
+                        INNER JOIN `users` ON `users`.`id` = `comments`.`user_id` WHERE `comments`.`article_id`={$id}";
+			$comment_query = mysqli_query($connection, $query1);
+			
+            while ($row = mysqli_fetch_assoc($comment_query)) {
+                  $name = $row['first_name'];
+                  $last_name = $row['last_name'];
+                  $comment = $row['descriptions']; 
+                  $date = $row['datetimee'];
+                  echo "<div class='col-md-8'>
+                  <div class='media g-mb-30 media-comment'>
+                      <img class='d-flex g-width-50 g-height-50 rounded-circle g-mt-3 g-mr-15' src='https://bootdey.com/img/Content/avatar/avatar7.png' alt='Image Description'>
+                      <div class='media-body u-shadow-v18 g-bg-secondary g-pa-30'>
+                      
+                        <div class='g-mb-15'>
+                          <h5 class='h5 g-color-gray-dark-v1 mb-0'>{$name} {$last_name}</h5>
+                          <span class='g-color-gray-dark-v4 g-font-size-12'>{$date}</span>
+                        </div>
+                        <div class='komenti'>
+                        <p>{$comment}</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>";}
+}
 
 function esc(String $value){
 	// bring the global db connect object into function
@@ -27,5 +54,4 @@ function esc(String $value){
 
 	return $val;
 }
-
 ?>

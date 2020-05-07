06b2aa95b859bc5bed1973bb883b532f339bff89
rrustemh <58404371+rrustemh@users.noreply.github.com>
2020-05-08 00:27:01 +0200

    Update favorite.php

--- favorite.php
+++ favorite.php
@@ -5,7 +5,7 @@
     if(isset($_POST['deleteid']) && isset($_POST['userid'])){
         $postid = $_POST['deleteid'];
         $userid = $_POST['userid'];
-        $query = "DELETE FROM bookmarks WHERE userid=$userid AND article_id = $postid;";
+        $query = "DELETE FROM bookmarks WHERE user_id=$userid AND article_id = $postid;";
         $delete = mysqli_query($connection, $query);
         if(!$delete){
             die("Error: " + mysqli_error($connection));
@@ -14,7 +14,7 @@
     if(isset($_POST['insertid']) && isset($_POST['userid'])){
         $postid = $_POST['insertid'];
         $userid = $_POST['userid'];
-        $query = "INSERT INTO bookmarks(userid, article_id) VALUES ($userid, $postid);";
+        $query = "INSERT INTO bookmarks(user_id, article_id) VALUES ($userid, $postid);";
         $insert = mysqli_query($connection, $query);
         if(!$insert){
             die("Error: " + mysqli_error($connection));

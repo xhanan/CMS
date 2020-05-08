d5b9f97a60a1fbdf99eb2d46e8e2e18cb4472d0b
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-08 22:33:25 +0200

    most_viewed_post in Blog.php

--- blog.php
+++ blog.php
@@ -33,13 +33,8 @@
                     <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
                 </div>
                 <div class="row pb-3">
-                    <div class="col-5 align-self-center">
-                        <img src="images/download (1).jpg" alt="img" class="fh5co_most_trading"/>
-                    </div>
-                    <div class="col-7 paddding">
-                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                    </div>
+                    <?php most_viewed_posts();?>
+                    
                 </div>
             </div>
         </div>
--- index.php
+++ index.php
@@ -70,41 +70,7 @@
                         <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
                     </div>
                     <?php most_viewed_posts();?>
-                    <!-- <div class="row pb-3">
-                        <div class="col-5 align-self-center">
-                            <img src="images/download (1).jpg" alt="img" class="fh5co_most_trading" />
-                        </div>
-                        <div class="col-7 paddding">
-                            <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                            <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                        </div>
                     </div>
-                    <div class="row pb-3">
-                        <div class="col-5 align-self-center">
-                            <img src="images/allef-vinicius-108153.jpg" alt="img" class="fh5co_most_trading" />
-                        </div>
-                        <div class="col-7 paddding">
-                            <div class="most_fh5co_treding_font"> Enim ad minim veniam nostrud xercitation ullamco.</div>
-                            <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                        </div>
-                    </div>
-                    <div class="row pb-3">
-                        <div class="col-5 align-self-center">
-                            <img src="images/download (2).jpg" alt="img" class="fh5co_most_trading" />
-                        </div>
-                        <div class="col-7 paddding">
-                            <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                            <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                        </div>
-                    </div>
-                    <div class="row pb-3">
-                        <div class="col-5 align-self-center"><img src="images/seth-doyle-133175.jpg" alt="img" class="fh5co_most_trading" /></div>
-                        <div class="col-7 paddding">
-                            <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
-                            <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
-                        </div>
-                    </div> -->
-                </div>
             </div>
             <div class="row mx-0 animate-box" data-animate-effect="fadeInUp">
                 <div class="col-12 text-center pb-4 pt-4">

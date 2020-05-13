9005fc7638d91670032c14bf7f3ed3470e20c498
rrustemh <58404371+rrustemh@users.noreply.github.com>
2020-05-13 21:38:28 +0200

    Update oopsignup.php

--- oopsignup.php
+++ oopsignup.php
@@ -6,19 +6,21 @@
         private $email;
         private $username;
         private $password;
+        private $gender;
     
-        function __construct($firstname, $lastname, $email, $username, $password){
+        function __construct($firstname, $lastname, $email, $username, $password, $gender){
             $this->firstname = $firstname;
             $this->lastname = $lastname;
             $this->email = $email;
             $this->username = $username;
             $this->password = $password;
+            $this->gender = $gender;
         }
         function insertUsers(){
             global $connection;
             
-            $query = "INSERT INTO users (first_name, last_name,email, username, passwordd)";
-            $query .= "VALUES('$this->firstname','$this->lastname','$this->email','$this->username','$this->password')";
+            $query = "INSERT INTO users (first_name, last_name,email, username, passwordd, gender)";
+            $query .= "VALUES('$this->firstname','$this->lastname','$this->email','$this->username','$this->password','$this->gender')";
             $insert = mysqli_query($connection, $query);
             if(!$insert){
                 die("Couldn't connect to the database ".mysqli_error($connection));

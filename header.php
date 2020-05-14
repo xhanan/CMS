bcda34411c99fd24915c73fb65b4184723afbc66
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-14 18:42:42 +0200

    Update header.php

--- header.php
+++ header.php
@@ -125,7 +125,7 @@
                 <button class="navbar-toggler navbar-toggler-right mt-3" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
                 <a class="navbar-brand" href="#"><img src="images/logo.png" alt="img" class="mobile_logo_width" /></a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto" style="margin-left: 75px;">
+                    <ul class="navbar-nav mr-auto" style="margin-left: 18%;">
                         <li class="nav-item <?php if(strpos($_SERVER['REQUEST_URI'], '/cms/index') !== false) { echo "active"; } ?>">
                             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                         </li>

e73a605c9a69d2ff29349f927e7c8bcd36ac177f
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-14 18:26:08 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,10 +5,8 @@
 <?php
     include "cookies.php";
     if(!isset($_COOKIE['brainbyte'])){
-        setcookie("brainbyte",cookies::generateCookieValue(), time() + 3600, '/');
+        setcookie("brainbyte",cookies::generateCookieValue(), time() + (3600*730.001), '/');
     }
-
-    echo $_COOKIE['brainbyte'];
 ?>
 <head>
     <!-- Required meta tags -->
@@ -127,7 +125,7 @@
                 <button class="navbar-toggler navbar-toggler-right mt-3" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
                 <a class="navbar-brand" href="#"><img src="images/logo.png" alt="img" class="mobile_logo_width" /></a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav mr-auto" style="margin-left: 75px;">
                         <li class="nav-item <?php if(strpos($_SERVER['REQUEST_URI'], '/cms/index') !== false) { echo "active"; } ?>">
                             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                         </li>
